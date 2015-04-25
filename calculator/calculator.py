9c150a0f2e3e4efda7353d1a72ed6455453a357a
skiwi <frank_skiwi@hotmail.com>
2015-04-25 15:10:26 +0200

    Added type hinting.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -2,11 +2,12 @@ from decimal import Decimal
 
 __author__ = 'Frank van Heeswijk'
 
+
 class Calculator:
 
-    def evaluate(self, expression):
+    def evaluate(self, expression: str) -> Decimal:
         """
-        Evaluates an expression and returns its result as a decimal.
+        Evaluates an expression and returns its result.
 
         :param expression:  The input string
         :return:    A decimal representing the string.
