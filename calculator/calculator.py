8bf09ac8171df6bfffc0597dceafac4bec72dfd6
skiwi <frank_skiwi@hotmail.com>
2015-04-25 17:28:35 +0200

    Added method to tokenize an expression into a list of tokens in Reverse Polish Notation form.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,15 +1,116 @@
+from collections import deque
 from decimal import Decimal
+import re
 
 __author__ = 'Frank van Heeswijk'
 
 
 class Calculator:
-
     def evaluate(self, expression: str) -> Decimal:
         """
         Evaluates an expression and returns its result.
 
-        :param expression:  The input string
-        :return:    A decimal representing the string.
+        :param expression:  The input expression
+        :return:    The output of evaluating the expression
         """
         return Decimal(expression)
+
+    def tokenize(self, expression: str) -> list:
+        """
+        Tokenizes an expression and produces an output list in Reverse Polish Notation form.
+
+        :rtype: list of [Token]
+
+        :param expression:  The input expression
+        :raise RuntimeError:    If the parentheses are mismatched
+        """
+        output_queue = []
+        stack = []
+        # todo do not depend on spaces anymore
+        tokens = expression.split(' ')
+
+        decimal_regex = re.compile(r"^-?\d+(\.\d+)?$")
+
+        for token in tokens:
+            if decimal_regex.match(token):
+                output_queue.append(ValueToken(Decimal(token)))
+            elif token == '(':
+                stack.append(LeftParenthesesToken())
+            elif token == ')':
+                while len(stack) > 0:
+                    pop_token = stack.pop()
+                    if isinstance(pop_token, LeftParenthesesToken):
+                        break
+                    output_queue.append(pop_token)
+                    # todo implement function support
+                else:
+                    raise RuntimeError("mismatched parentheses")
+
+            else:
+                # todo implement precedence rules
+                stack.append(OperatorToken(token))
+
+        while len(stack) > 0:
+            pop_token = stack.pop()
+            if isinstance(pop_token, LeftParenthesesToken):
+                raise RuntimeError("mismatched parentheses")
+            output_queue.append(pop_token)
+
+        return output_queue
+
+
+class Token:
+    pass
+
+
+class ValueToken(Token):
+    def __init__(self, value: Decimal):
+        self.value = value
+
+    def __repr__(self):
+        return "VT(" + str(self.value) + ")"
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.value == other.value
+
+    def __ne__(self, other):
+        return not self == other
+
+
+class OperatorToken(Token):
+    def __init__(self, operator: str):
+        self.operator = operator
+
+    def __repr__(self):
+        return "OT(" + self.operator + ")"
+
+    def __hash__(self):
+        return hash(str)
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return self.operator == other.operator
+
+    def __ne__(self, other):
+        return not self == other
+
+class LeftParenthesesToken(Token):
+    def __repr__(self):
+        return "LPT"
+
+    def __hash__(self):
+        return 0
+
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return False
+        return True
+
+    def __ne__(self, other):
+        return not self == other
--- calculator/test/test_calculator.py
+++ calculator/test/test_calculator.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from calculator.calculator import Calculator
+from calculator.calculator import Calculator, ValueToken, OperatorToken
 
 __author__ = 'Frank van Heeswijk'
 
@@ -9,6 +9,75 @@ class CalculatorTest(unittest.TestCase):
     def test_evaluate(self):
         calculator = Calculator()
         self.assertEqual(Decimal(4), calculator.evaluate("4"))
+
+    def test_tokenize_value(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal(2))
+        ]
+        self.assertListEqual(expected, calculator.tokenize("2"))
+
+    def test_tokenize_value_parentheses(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal(2))
+        ]
+        self.assertListEqual(expected, calculator.tokenize("( 2 )"))
+
+    def test_tokenize_value_parentheses_missing_right(self):
+        calculator = Calculator()
+        self.assertRaises(RuntimeError, calculator.tokenize, "2 )")
+
+    def test_tokenize_value_parentheses_missing_left(self):
+        calculator = Calculator()
+        self.assertRaises(RuntimeError, calculator.tokenize, "( 2")
+
+    def test_tokenize_decimal(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal("6.2"))
+        ]
+        self.assertListEqual(expected, calculator.tokenize("6.2"))
+
+    def test_tokenize_operator(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal(2)),
+            ValueToken(Decimal(4)),
+            OperatorToken('+')
+        ]
+        self.assertListEqual(expected, calculator.tokenize("2 + 4"))
+
+    def test_tokenize_operator_parentheses(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal(2)),
+            ValueToken(Decimal(4)),
+            OperatorToken('+'),
+        ]
+        self.assertListEqual(expected, calculator.tokenize("( 2 + 4 )"))
+
+    def test_tokenize_multiple_parentheses(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal(3)),
+            ValueToken(Decimal(2)),
+            ValueToken(Decimal(4)),
+            OperatorToken('+'),
+            OperatorToken('*')
+        ]
+        self.assertListEqual(expected, calculator.tokenize("( 3 * ( ( 2 ) + ( 4 ) ) )"))
+
+    def test_tokenize_multiple_parentheses_decimal(self):
+        calculator = Calculator()
+        expected = [
+            ValueToken(Decimal("3.5")),
+            ValueToken(Decimal("2.7")),
+            ValueToken(Decimal("4.8")),
+            OperatorToken('+'),
+            OperatorToken('*')
+        ]
+        self.assertListEqual(expected, calculator.tokenize("( 3.5 * ( ( 2.7 ) + ( 4.8 ) ) )"))
 
 if __name__ == '__main__':
     unittest.main()
